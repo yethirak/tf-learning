59a16f02bbd721bfb22192e633ed06811ed9f815
Akhilesh Yethirajam <33382779+yethirak@users.noreply.github.com>
2023-10-22 13:34:09 +0800

    1: Ignore localoutputs 2: move location to output directory

--- lesson01/localfile.tf
+++ lesson01/localfile.tf
@@ -1,4 +1,4 @@
 resource "local_file" "pet" {
-    filename = "E:\\learnings\\tf-learning\\lesson01\\pet.txt"
+    filename = "E:\\learnings\\tf-learning\\lesson01\\output\\pet.txt"
      content = "I love Pets!"
 }
